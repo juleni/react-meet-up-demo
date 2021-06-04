ad508741ff672ee345a0bb7f5ffbea231a0c1b80
juleni <69912179+juleni@users.noreply.github.com>
2021-06-04 13:41:12 +0200

    Added MeetupItem component

--- src/components/meetups/MeetupList.js
+++ src/components/meetups/MeetupList.js
@@ -1,8 +1,19 @@
-function MeetupList() {
-  render (
-    <div>
+import MeetupItem from "./MeetupItem";
+import classes from "./MeetupList.module.css";
 
-    </div>
+function MeetupList(props) {
+  return (
+    <ul className={classes.list}>
+      {props.meetups.map(meetup => (
+        <MeetupItem 
+          key={meetup.id} 
+          id={meetup.id} 
+          image={meetup.image} 
+          title={meetup.title}
+          address={meetup.address}
+          description={meetup.description}
+        />))}
+    </ul>
   );
 }
 
